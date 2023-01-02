030b72ec98304e81268362579bc27d45760926dd
Nitin Londhe <nitin.londhe@genmills.com>
2023-01-02 22:46:01 +0530

    efficient code refactor using closures and iterators

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use std::{env, process};
 use minigrep::{Config, run};
 
 fn main() {
-    let args = env::args().collect::<Vec<String>>();
+    let args = env::args();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(args).unwrap_or_else(|err| {
         eprintln!("Problems parsing arguments : {err}");
         process::exit(1);
     });
