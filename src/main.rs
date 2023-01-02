217d8cd46697633643a9394ce86790c83b8e3b30
Nitin Londhe <nitin.londhe@genmills.com>
2023-01-02 19:15:21 +0530

    added lib crate

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::{env, fs, process, error::Error};
+use std::{env, process};
+
+use minigrep::{Config, run};
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
@@ -14,35 +16,5 @@ fn main() {
     if let Err(e) = run(config) {
         println!("Application error: {e}");
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs:: read_to_string(config.file_path)?;
-    println!("With text: \n {contents}");
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &str> {
-
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        
-        let config = Config { 
-            query, 
-            file_path 
-        };
-
-        Ok(config)
     }
 }
